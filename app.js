44767fdcc4a4a7c13320f7fdf7dac5273b8c77df
Mike Keohane <mkeohane@gatech.edu>
2019-03-01 00:57:35 -0500

    added title screen and game over mechanics

--- app.js
+++ app.js
@@ -16,7 +16,11 @@ var background = new Image();
 background.src = "background.png"
 var obstacle = new Image();
 obstacle.src = "rock.png"
+var titleImage = new Image();
+titleImage.src = "title.jpg"
 var speed = 40;
+var gameActive = false;
+var isTitle = true;
 
 var blocks = [{x: 480, y: 285, width: 45, height: 25, updateSpot : function() {
     this.x = this.x - 5;
@@ -27,89 +31,101 @@ var player = {x: 15, y: 270, width: 50, height: 50, updateSpot : function() {
   }}
 
   function draw() {
-    if (count % 5 == 0) {
-        score++;
-    }
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(background, 0, 0);
-    ctx.font = "18px Arial";
-    ctx.fillStyle = "white";
-    ctx.fillText("Score: " + score, 360, 30);
-
-    // update player based on keys
-    if (leftPressed && player.x >= 5) {
-        player.x -= 8;
-    }
-    if (rightPressed && player.x <= 430) {
-        player.x += 8;
-    }
-    if (upPressed || jump != 10) {
-        if (goingUp) {
-            jump--;
-            if (jump >= 7) {
-                player.y -= 17;
-            } else if (jump >= 4) {
-                player.y -= 12;
-            } else {
-                player.y -= 7;
+    if (gameActive) {
+        if (count % 5 == 0) {
+            score++;
+        }
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(background, 0, 0);
+        ctx.font = "18px Arial";
+        ctx.fillStyle = "white";
+        ctx.fillText("Score: " + score, 360, 30);
+    
+        // update player based on keys
+        if (leftPressed && player.x >= 5) {
+            player.x -= 8;
+        }
+        if (rightPressed && player.x <= 430) {
+            player.x += 8;
+        }
+        if (upPressed || jump != 10) {
+            if (goingUp) {
+                jump--;
+                if (jump >= 7) {
+                    player.y -= 17;
+                } else if (jump >= 4) {
+                    player.y -= 12;
+                } else {
+                    player.y -= 7;
+                }
+                
+                if (jump == 0) {
+                    goingUp = false;
+                }
+            } else if (jump < 10) {
+                jump++;
+                if (jump >= 7) {
+                    player.y += 7;
+                } else if (jump >= 4) {
+                    player.y += 12;
+                } else {
+                    player.y += 17;
+                }
+    
+                if (jump == 10) {
+                    goingUp = true;
+                }
             }
+        }
+    
+        // draw player
+        ctx.drawImage(image, player.x, player.y);
+    
+        // draw blocks
+        blocks.forEach(function(element) {
+            element.updateSpot();
+            if (element.x > -50) {
+                ctx.beginPath();
+                ctx.drawImage(obstacle, element.x, element.y)
+                // ctx.rect(element.x, element.y, element.width, element.height);
+                // ctx.fillStyle = "#FF0000";
+                // ctx.fill();
+                ctx.closePath();
+            } else {
+                blocks.splice(element,1);
+            }
+            checkCollision(element);
             
-            if (jump == 0) {
-                goingUp = false;
-            }
-        } else if (jump < 10) {
-            jump++;
-            if (jump >= 7) {
-                player.y += 7;
-            } else if (jump >= 4) {
-                player.y += 12;
-            } else {
-                player.y += 17;
-            }
-
-            if (jump == 10) {
-                goingUp = true;
+          });
+          
+        count = count + 1;
+        if (count % max == 0) {
+            blocks.push({x: 480, y: 285, width: 45, height: 25, updateSpot : function() {
+                this.x = this.x - 5;
+              }})
+        }
+        if (count % 100 == 0) {
+            if (speed > 0){
+                speed -= 5;
             }
         }
-    }
+        if (count % 150 == 0) {
+            if (max > 10) {
+                max -= 10;
+            }
+        }
+        console.log("looped");
+    } else if (isTitle) {
+        ctx.drawImage(titleImage, 0, -20);
+        ctx.font = "60px Arial";
+        ctx.fillStyle = "red";
+        ctx.fillText("JUMP APE", canvas.width / 5, 2 * canvas.height / 3 - 15);
+        ctx.font = "20px Arial";
+        ctx.fillStyle = "white";
+        ctx.fillText("( press enter to begin )", canvas.width / 3.5, 3 * canvas.height / 4 - 15);
 
-    // draw player
-    ctx.drawImage(image, player.x, player.y);
-
-    // draw blocks
-    blocks.forEach(function(element) {
-        element.updateSpot();
-        if (element.x > -50) {
-            ctx.beginPath();
-            ctx.drawImage(obstacle, element.x, element.y)
-            // ctx.rect(element.x, element.y, element.width, element.height);
-            // ctx.fillStyle = "#FF0000";
-            // ctx.fill();
-            ctx.closePath();
-        } else {
-            blocks.splice(element,1);
-        }
-        checkCollision(element);
-        
-      });
-      
-    count = count + 1;
-    if (count % max == 0) {
-        blocks.push({x: 480, y: 285, width: 45, height: 25, updateSpot : function() {
-            this.x = this.x - 5;
-          }})
     }
-    if (count % 100 == 0) {
-        if (speed > 0){
-            speed -= 5;
-        }
-    }
-    if (count % 150 == 0) {
-        if (max > 10) {
-            max -= 10;
-        }
-    }
-    console.log("looped");
+    
 }
 var interval = setInterval(draw, speed);
 
@@ -122,6 +138,20 @@ function keyDownHandler(e) {
         leftPressed = true;
     } else if(e.key =="Up" || e.key == "ArrowUp") {
         upPressed = true;
+    } else if (e.key == "Enter") {
+        gameActive = true;
+        isTitle = false;
+        score = 0;
+        jump = 10;
+        speed = 40;
+        count = 0;
+        blocks = [{x: 480, y: 285, width: 45, height: 25, updateSpot : function() {
+            this.x = this.x - 5;
+          }}]
+        
+        player = {x: 15, y: 270, width: 50, height: 50, updateSpot : function() {
+            this.x = this.x - 5;
+          }}
     }
 }
 
@@ -142,19 +172,26 @@ function checkCollision(element) {
     if (element.x > player.x && element.x < (player.x + 45)) {
         // check y
         if (element.y > player.y && element.y < player.y + 43) {
-            alert("GAME OVER\nYOUR SCORE: " + score);
-            score = 0;
-            document.location.reload();
-            clearInterval(interval); // Needed for Chrome to end game
+            gameOver();
         }
     }
     if (element.x + 50 > player.x && element.x + 50 < (player.x + 48)) {
         // check y
         if (element.y > player.y && element.y < player.y + 43) {
-            alert("GAME OVER\nYOUR SCORE: " + score);
-            score = 0;
-            document.location.reload();
-            clearInterval(interval); // Needed for Chrome to end game
+            gameOver();
         }
     }
+}
+
+function gameOver() {
+    gameActive = false;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "black";
+    ctx.fillRect(0,0,canvas.width, canvas.height);
+    ctx.font = "60px Arial";
+    ctx.fillStyle = "white";
+    ctx.fillText("Game Over", canvas.width / 5, 2 * canvas.height / 3 - 45);
+    ctx.font = "20px Arial";
+    ctx.fillText("Score: " + score, canvas.width / 2.3, 3 * canvas.height / 4 - 35);
+    ctx.fillText("( press enter to retry )", canvas.width / 3.3, 3 * canvas.height / 4 - 15);
 }
